7c0bca4683bf7e54c4f4102db7bb377acb1f6a65
ze-kel <k.kelolov@gmail.com>
2021-03-27 17:07:27 +0300

    4.8-4.14

--- part4/blogBackend/controllers/blogs.js
+++ part4/blogBackend/controllers/blogs.js
@@ -1,57 +1,52 @@
 const blogsRouter = require('express').Router()
 const Blog = require("../models/blog")
 
-blogsRouter.get('/', (request, response) => {
-    Blog
-        .find({})
-        .then(blogs => {
-            response.json(blogs)
-        })
+blogsRouter.get('/', async (request, response) => {
+    const blogs = await Blog.find({})
+    response.json(blogs)
 })
 
-blogsRouter.post('/', (request, response) => {
-    const blog = new Blog(request.body)
-    blog
-        .save()
-        .then(result => {
-            response.status(201).json(result)
-        })
+blogsRouter.post('/', async (request, response) => {
+    if (!request.body.title || !request.body.author) {
+        response.status(400).end()
+    } else {
+        const blog = new Blog(request.body)
+        if (!blog.likes) {
+            blog.likes = 0
+        }
+        const result = await blog.save()
+        response.status(201).json(result)
+    }
 })
 
-blogsRouter.get('/:id', (request, response, next) => {
-    Blog.findById(request.params.id)
-        .then(blog => {
-            if (blog) {
-                response.json(blog)
-            } else {
-                response.status(404).end()
-            }
-        })
-        .catch(error => next(error))
+blogsRouter.get('/:id', async (request, response) => {
+    const blog = await Blog.findById(request.params.id)
+    if (blog) {
+        response.json(blog)
+    } else {
+        response.status(404).end()
+    }
 })
 
-blogsRouter.delete('/:id', (request, response, next) => {
-    Blog.findByIdAndRemove(request.params.id)
-        .then(() => {
-            response.status(204).end()
-        })
-        .catch(error => next(error))
+blogsRouter.delete('/:id', async (request, response) => {
+    await Blog.findByIdAndRemove(request.params.id)
+
+    response.status(204).end()
 })
 
-blogsRouter.put('/:id', (request, response, next) => {
+blogsRouter.put('/:id', async (request, response) => {
     const body = request.body
 
     const blog = {
         title: body.title,
-        url: body.ulr,
+        author: body.author,
+        url: body.url,
         likes: body.likes
     }
 
-    Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
-        .then(updatedblog => {
-            response.json(updatedblog)
-        })
-        .catch(error => next(error))
+    const updatedblog = await Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
+
+    response.json(updatedblog)
 })
 
 
---
+++ part4/blogBackend/tests/test.helper.js
@@ -0,0 +1,28 @@
+const initialBlogs = [
+    {
+        _id: '5a444aa71b54a676234d17f8',
+        title: 'Lorem',
+        author: 'Jake',
+        url: 'someurl',
+        likes: 28,
+        __v: 0
+    },
+    {
+        _id: '5a333aa71b54a676234d17f8',
+        title: 'Mum',
+        author: 'Pyotr',
+        url: 'someurl',
+        likes: 50,
+        __v: 0
+    },
+    {
+        _id: '5a222aa71b54a676234d17f8',
+        title: 'Stuff',
+        author: 'Vasya',
+        url: 'someurl',
+        likes: 10,
+        __v: 0
+    }
+]
+
+module.exports = { initialBlogs }
