cb9d1504711ea6412fcda342bc6f2c185b92eb91
ze-kel <k.kelolov@gmail.com>
2021-03-30 16:59:15 +0300

    7.1-7.8

---
+++ part7/routed-anecdotes/src/hooks/index.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react'
+
+
+export const useField = (type) => {
+    const [value, setValue] = useState('')
+
+    const onChange = (event) => {
+        setValue(event.target.value)
+    }
+
+    const reset = () => {
+        setValue('')
+    }
+
+    return {
+        input: {
+            type,
+            value,
+            onChange,
+        },
+        reset: reset
+    }
+}
